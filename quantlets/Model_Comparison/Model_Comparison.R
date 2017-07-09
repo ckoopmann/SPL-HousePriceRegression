2be20f388e4d0a8396ee214fec982e1865be0172
ckoopmann <c.k.e.koopmann@gmail.com>
2017-07-09 18:42:00 +0200

    Changes to Model_Comparison

--- quantlets/Model_Comparison/Model_Comparison.R
+++ quantlets/Model_Comparison/Model_Comparison.R
@@ -5,179 +5,189 @@ graphics.off()
 # Install and load packages
 libraries = c("ggplot2", "xtable", "gridExtra", "gbm", "plyr", "glmnet")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-      install.packages(x)
+    install.packages(x)
 })
 
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-#was machen wir hier eigentlich korrekterweise?
+# setting up working directory
 setwd("C:/Users/Felix/Uni/SPL/HousePriceRegression/quantlets/Model_Comparison")
 
 # Read in pre-processed Data:
 train = read.csv("train_preprocessed.csv")
-test = read.csv("test_preprocessed.csv")
+test  = read.csv("test_preprocessed.csv")
 
 # Read in trained models
-load("rf.RData")                    # random forest
-load("gbm.RData")                   # gradient boosting model
-load("regression_models_fit.RData") # OLS based models
+load("rf.RData")  # random forest
+load("gbm.RData")  # gradient boosting model
+load("regression_models_fit.RData")  # OLS based models
 
 
-#set rownumbers in dataframe to NULL
+# set rownumbers in dataframe to NULL
 train$X  = NULL
 train$Id = NULL
 test$X   = NULL
 test$Id  = NULL
 
 # Comparing the models by mean squared error (MSE)
-model.mse = function(model,test.data = test){
-    if(class(model)[1] %in% c("train","lm")){
+model.mse = function(model, test.data = test) {
+    if (class(model)[1] %in% c("train", "lm")) {
         pred = predict(model, newdata = test.data)
-        mse  = (1/ncol(test.data))*sum((pred - test.data$logSalePrice)^2)
-    }else{
-        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s ="lambda.1se")
-        mse  = (1/ncol(test.data))*sum((pred - test.data$logSalePrice)^2)
+        mse  = (1/ncol(test.data)) * sum((pred - test.data$logSalePrice)^2)
+    } else {
+        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s = "lambda.1se")
+        mse  = (1/ncol(test.data)) * sum((pred - test.data$logSalePrice)^2)
     }
     return(mse)
 }
 
-model_overfit.mse = function(model,test.data = train){
-      if(class(model)[1] %in% c("train","lm")){
-            pred = predict(model, newdata = test.data)
-            mse  = (1/ncol(test.data))*sum((pred - test.data$logSalePrice)^2)
-      }else{
-            pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s ="lambda.1se")
-            mse  = (1/ncol(test.data))*sum((pred - test.data$logSalePrice)^2)
-      }
-      return(mse)
+# Comparing the models by MSE on the training data
+model_overfit.mse = function(model, test.data = train) {
+    if (class(model)[1] %in% c("train", "lm")) {
+        pred = predict(model, newdata = test.data)
+        mse  = (1/ncol(test.data)) * sum((pred - test.data$logSalePrice)^2)
+    } else {
+        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s = "lambda.1se")
+        mse  = (1/ncol(test.data)) * sum((pred - test.data$logSalePrice)^2)
+    }
+    return(mse)
 }
 
 
 # Comparing the models by mean absolute errror
-model.mae = function(model,test.data = test){
-      if(class(model)[1] %in% c("train","lm")){
-            pred = predict(model, newdata = test.data)
-            mae  = (1/ncol(test.data))*sum(abs(pred - test.data$logSalePrice))
-      }else{
-            pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s ="lambda.1se")
-            mae  = (1/ncol(test.data))*sum(abs(pred - test.data$logSalePrice))
-      }
-      return(mae)
+model.mae = function(model, test.data = test) {
+    if (class(model)[1] %in% c("train", "lm")) {
+        pred = predict(model, newdata = test.data)
+        mae  = (1/ncol(test.data)) * sum(abs(pred - test.data$logSalePrice))
+    } else {
+        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s = "lambda.1se")
+        mae  = (1/ncol(test.data)) * sum(abs(pred - test.data$logSalePrice))
+    }
+    return(mae)
 }
 
 # Comparing the models by bias
-model.bias = function(model,test.data = test){
-      if(class(model)[1] %in% c("train","lm")){
-            pred = predict(model, newdata = test.data)
-            bias  = (1/ncol(test.data))*sum(pred - test.data$logSalePrice)
-      }else{
-            pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s ="lambda.1se")
-            bias  = (1/ncol(test.data))*sum(pred - test.data$logSalePrice)
-      }
-      return(bias)
+model.bias = function(model, test.data = test) {
+    if (class(model)[1] %in% c("train", "lm")) {
+        pred = predict(model, newdata = test.data)
+        bias = (1/ncol(test.data)) * sum(pred - test.data$logSalePrice)
+    } else {
+        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s = "lambda.1se")
+        bias = (1/ncol(test.data)) * sum(pred - test.data$logSalePrice)
+    }
+    return(bias)
 }
 
 # Comparing the models by R^2
-model.Rsq = function(model,test.data = test){
-      if(class(model)[1] %in% c("train","lm")){
-            pred = predict(model, newdata = test.data)
-            Rsq  = 1 - sum((pred - test.data$logSalePrice)^2)/sum((test.data$logSalePrice - mean(test.data$logSalePrice))^2)
-      }else{
-            pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s ="lambda.1se")
-            Rsq  = 1 - sum((pred - test.data$logSalePrice)^2)/sum((test.data$logSalePrice - mean(test.data$logSalePrice))^2)
-      }
-      return(Rsq)
+model.Rsq = function(model, test.data = test) {
+    if (class(model)[1] %in% c("train", "lm")) {
+        pred = predict(model, newdata = test.data)
+        Rsq  = 1 - sum((test.data$logSalePrice - pred)^2)/sum((test.data$logSalePrice - mean(test.data$logSalePrice))^2)
+    } else {
+        pred = predict(model, newx = as.matrix(test.data[!names(test.data) %in% "logSalePrice"]), s = "lambda.1se")
+        Rsq  = 1 - sum((test.data$logSalePrice - pred)^2)/sum((test.data$logSalePrice - mean(test.data$logSalePrice))^2)
+    }
+    return(Rsq)
 }
 
 # comparing of the models using different measures
-model.list              = list(lm.fit,fwd.fit,lasso.fit,ridge.fit,gbmtuned,rftuned)
-comparisonMSE.list      = sapply(model.list, FUN = model.mse)
-comparisonMSEtrain.list = sapply(model.list, FUN = model_overfit.mse)
-comparisonMAE.list      = sapply(model.list, FUN = model.mae)
-comparisonBIAS.list     = sapply(model.list, FUN = model.bias)
-comparisonRSQ.list      = sapply(model.list, FUN = model.Rsq)
-results                 = matrix(c(round(comparisonMSE.list,3),round(comparisonMSEtrain.list,3),round(comparisonMAE.list,3),round(comparisonBIAS.list,3),round(comparisonRSQ.list,3)),ncol = length(model.list), byrow = TRUE)
-rownames(results)       = c("MSE","MSEtrain", "MAE","BIAS","RSQ")
-colnames(results)       = c("lm","fwd","lasso","ridge","gbm","rf")
-results
+model.list                  = list(lm.fit, fwd.fit, lasso.fit, ridge.fit, gbmtuned, rftuned)
+comparisonMSE.list          = sapply(model.list, FUN = model.mse)
+comparisonMSEtrain.list     = sapply(model.list, FUN = model_overfit.mse)
+comparisonMAE.list          = sapply(model.list, FUN = model.mae)
+comparisonBIAS.list         = sapply(model.list, FUN = model.bias)
+comparisonRSQ.list          = sapply(model.list, FUN = model.Rsq)
+comparison.result           = matrix(c(round(comparisonMSE.list, 3), round(comparisonMSEtrain.list, 3), round(comparisonMAE.list, 3), 
+    round(comparisonBIAS.list, 3), round(comparisonRSQ.list, 3)), ncol = length(model.list), byrow = TRUE)
+rownames(comparison.result) = c("MSE", "MSEtrain", "MAE", "BIAS", "RSQ")
+colnames(comparison.result) = c("lm", "fwd", "lasso", "ridge", "gbm", "rf")
+comparison.result
 
-modelcomparison_latex = xtable(results)
+# Writing a latex table containing the model comparison results
+modelcomparison_latex = xtable(comparison.result)
 print(modelcomparison_latex, file = "modelcomparison.tex")
 
+#################################################################################
+# Plotting the estimations against the real values in the test dataset
 
-# plot real values against estimated values
-# lm.fit plot
-df.lm.fit = data.frame(cbind(test$logSalePrice,predict(lm.fit, newdata = test)))
-#pdf(file = "lm.fit_realVsfitted.pdf")
-lm.plot = ggplot(df.lm.fit,aes(test$logSalePrice,predict(lm.fit, newdata = test))) + geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "lm.fit predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","lm"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","lm"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+
+# plot real values against estimated values lm.fit plot
+predictions.lm = predict(lm.fit, newdata = test)                        # predicting the outcome for test data
+df.lm.fit      = data.frame(cbind(test$logSalePrice, predictions.lm ))  # creating dataframe containing real and predicted outcome
+
+lm.plot = ggplot(df.lm.fit, aes(test$logSalePrice, predictions.lm)) + geom_point() + geom_segment(x = -4, 
+    y = -4, xend = 4, yend = 4, color = "red", size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "lm.fit predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "lm"], sep = " "), x = -3, y = 3) + annotate("text", 
+    label = paste("MAE:", comparison.result["MAE", "lm"], sep = " "), x = -3, y = 2.5)
+
 
 # fwd.fit plot
-df.fwd.fit = data.frame(cbind(test$logSalePrice,predict(fwd.fit, newdata = test)))
-#pdf(file = "fwd.fit_realVsfitted.pdf")
-fwd.plot = ggplot(df.fwd.fit,aes(test$logSalePrice,predict(fwd.fit, newdata = test))) + geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "fwd.fit predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","fwd"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","fwd"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+predictions.fwd = predict(fwd.fit, newdata = test)
+df.fwd.fit      = data.frame(cbind(test$logSalePrice, predictions.fwd))
+
+fwd.plot = ggplot(df.fwd.fit, aes(test$logSalePrice, predictions.fwd)) + geom_point() + geom_segment(x = -4, 
+    y = -4, xend = 4, yend = 4, color = "red", size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "fwd.fit predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "fwd"], sep = " "), x = -3, y = 3) + 
+    annotate("text", label = paste("MAE:", comparison.result["MAE", "fwd"], sep = " "), x = -3, y = 2.5)
+
 
 # lasso.fit plot
-df.lasso.fit = data.frame(cbind(test$logSalePrice,predict(lasso.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s ="lambda.1se")))
-#pdf(file = "lasso.fit_realVsfitted.pdf")
-lasso.plot = ggplot(df.lasso.fit,aes(test$logSalePrice,predict(lasso.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s ="lambda.1se"))) + 
-    geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "lasso.fit predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","lasso"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","lasso"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+predictions.lasso = predict(lasso.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s = "lambda.1se")
+df.lasso.fit      = data.frame(cbind(test$logSalePrice, predictions.lasso))
+
+lasso.plot = ggplot(df.lasso.fit, aes(test$logSalePrice, predictions.lasso)) + geom_point() + geom_segment(x = -4, y = -4, xend = 4, yend = 4, color = "red", 
+    size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "lasso.fit predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "lasso"], sep = " "), x = -3, y = 3) + 
+    annotate("text", label = paste("MAE:", comparison.result["MAE", "lasso"], sep = " "), x = -3, y = 2.5)
+
 
 # ridge.fit plot
-df.ridge.fit = data.frame(cbind(test$logSalePrice,predict(ridge.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s ="lambda.1se")))
-#pdf(file = "ridge.fit_realVsfitted.pdf")
-ridge.plot = ggplot(df.ridge.fit,aes(test$logSalePrice,predict(ridge.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s ="lambda.1se"))) + 
-    geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "ridge.fit predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","ridge"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","ridge"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+predicions.ridge = predict(ridge.fit, newx = as.matrix(test[!names(test) %in% "logSalePrice"]), s = "lambda.1se")
+df.ridge.fit     = data.frame(cbind(test$logSalePrice, predicions.ridge))
+
+ridge.plot = ggplot(df.ridge.fit, aes(test$logSalePrice, predicions.ridge)) + geom_point() + geom_segment(x = -4, y = -4, xend = 4, yend = 4, color = "red", 
+    size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "ridge.fit predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "ridge"], sep = " "), x = -3, y = 3) + 
+    annotate("text", label = paste("MAE:", comparison.result["MAE", "ridge"], sep = " "), x = -3, y = 2.5)
+
 
 # gbm plot
-#prop.overestimated.gbm = (sum(predict(gbmtuned, newdata = test)-test$logSalePrice > 0)/length(test$logSalePrice))*100
-df.gbm = data.frame(cbind(test$logSalePrice,predict(gbmtuned, newdata = test)))
-#pdf(file = "gbm_realVsfitted.pdf")
-gbm.plot = ggplot(df.gbm,aes(test$logSalePrice,predict(gbmtuned, newdata = test))) + geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "gbmtuned predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","gbm"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","gbm"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+predictions.gbm = predict(gbmtuned, newdata = test)
+df.gbm          = data.frame(cbind(test$logSalePrice, predictions.gbm))
+
+gbm.plot = ggplot(df.gbm, aes(test$logSalePrice, predictions.gbm)) + geom_point() + geom_segment(x = -4, 
+    y = -4, xend = 4, yend = 4, color = "red", size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "gbmtuned predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "gbm"], sep = " "), x = -3, y = 3) + 
+    annotate("text", label = paste("MAE:", comparison.result["MAE", "gbm"], sep = " "), x = -3, y = 2.5)
+
 
 # rf plot
-#prop.overestimated.rf = (sum(predict(rftuned, newdata = test)-test$logSalePrice > 0)/length(test$logSalePrice))*100
-df.rf = data.frame(cbind(test$logSalePrice,predict(rftuned, newdata = test)))
-#pdf(file = "rf_realVsfitted.pdf")
-rf.plot = ggplot(df.rf,aes(test$logSalePrice,predict(rftuned, newdata = test))) + geom_point() + geom_segment(x = -4, y = -4,xend = 4, yend = 4, color = "red", size =1.3) +
-    stat_smooth(method="lm", se=FALSE) +
-    labs(title = "Plot of real logSalePrice against predicted values", x = "logSalePrice", y = "rftuned predictions") + 
-    theme(axis.title=element_text(size=16),  plot.title = element_text(size = 16, face = "bold")) +
-    annotate("text", label = paste("MSE:",results["MSE","rf"], sep = " "), x = -3, y = 3) +
-    annotate("text", label = paste("MAE:",results["MAE","rf"], sep = " "), x = -3, y = 2.5)
-#dev.off()
+predictions.rf = predict(rftuned, newdata = test)
+df.rf          = data.frame(cbind(test$logSalePrice, predictions.rf))
+
+rf.plot = ggplot(df.rf, aes(test$logSalePrice, predictions.rf)) + geom_point() + geom_segment(x = -4, 
+    y = -4, xend = 4, yend = 4, color = "red", size = 1.3) + stat_smooth(method = "lm", se = FALSE) + labs(title = "Plot of real logSalePrice against predicted values", 
+    x = "logSalePrice", y = "rftuned predictions") + theme(axis.title = element_text(size = 16), plot.title = element_text(size = 16, 
+    face = "bold")) + annotate("text", label = paste("MSE:", comparison.result["MSE", "rf"], sep = " "), x = -3, y = 3) + annotate("text", 
+    label = paste("MAE:", comparison.result["MAE", "rf"], sep = " "), x = -3, y = 2.5)
+
 
 # plotting resulting graphs together
-source("http://peterhaschke.com/Code/multiplot.R")
-pdf(file = "Model_Comparison.pdf")
-multiplot(lm.plot, fwd.plot, lasso.plot, ridge.plot, gbm.plot, ridge.plot, cols=2)
+source("multiplot.R")
+
+plot.list = list(lm.plot, fwd.plot, lasso.plot, ridge.plot, gbm.plot, rf.plot) # list of all graphs
+plot.loop = seq(length(plot.list))
+plot.loop = plot.loop[seq(length(plot.list)) %% 2 != 0]
+
+pdf("Model_comparison.pdf", onefile = TRUE)           
+for (i in plot.loop) {                                                         # looping through all models, creating a pdf with two plots per page for readability
+    j = i + 1
+    multiplot(plot.list[[i]], plot.list[[j]])  
+}
 dev.off()
+
