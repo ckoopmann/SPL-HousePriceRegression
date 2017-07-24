c1d1edbdc298e5f308751cdbb4651fd2354d65e4
Christian Karl Egbert Koopmann <KoopmaCK@basfad.basf.net>
2017-07-24 14:31:26 +0200

    Updated Random Forest Quantlets with new tuning grid and improved latex and pdf output

--- quantlets/Random_Forest/Random_Forest.R
+++ quantlets/Random_Forest/Random_Forest.R
@@ -5,14 +5,14 @@ ptrain      = 0.85
 nfolds      = 5
 nrepeats    = 1
 # Tuning Parameter Values for Random Forest
-mtry.tuning = c(10, 20, 30, 40, 50)
+mtry.tuning = seq(from = 20, to = 160, by = 20)
 # Tuning Paramter Values for Gradient Boosting Machine
 ntrees.tuning     = c(100, 500, 1000)
 intdepth.tuning   = 1
 shrinkage.tuning  = seq(0.01, 0.15, by = 0.01)
 minobs.tuning     = 10
 # install and load packages
-libraries = c("ggplot2", "randomForest", "caret", "doParallel", "gbm", "xtable", "rpart","rpart.plot")
+libraries = c("ggplot2", "randomForest", "caret", "doParallel", "gbm", "xtable", "rpart","rpart.plot", "Hmisc")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
     install.packages(x)
 })
@@ -27,15 +27,31 @@ rf                                  = randomForest(logSalePrice ~ ., data = df)
 importance                          = rf$importance[order(rf$importance, decreasing = TRUE), , drop = FALSE]
 importance.normalized               = as.data.frame(importance/importance[1])
 names(importance.normalized)[1]     = "Normalized_Importance"
-importance.normalized$variable      = row.names(importance.normalized)
 # Plot Variable Importance
 importance.plot = ggplot(data = importance.normalized[1:10, ], aes(x = variable, y = Normalized_Importance)) + geom_col() + 
-    scale_x_discrete(limits = importance.normalized[1:10, ]$variable) + ggtitle("Random Forest Variable Importance Top 10") + 
+    scale_x_discrete(limits = importance.normalized[1:10, ]$variable) + theme_classic() +  ggtitle("Random Forest Variable Importance Top 10") + 
     xlab("Variable") + ylab("Normalized Variable Importance")
 ggsave(importance.plot, filename = "rf_imp.pdf", width = 20, height = 10, units = 'cm')
 # Export Variable Importance as Latex Table
-importance.normalized_latex = xtable(importance.normalized)
-print(importance.normalized_latex, file = "importance.tex")
+rows.per.table = 30
+row.indices = seq(from = 1, to = nrow(importance.normalized), by = rows.per.table)
+latex.vector = character(0)
+for(i in 1:length(row.indices)){
+    cap = paste0("Random Forest Variable Importances Table:",i)
+    lab = paste0("tab:importance",i)
+    importance.normalized_latex = xtable(importance.normalized[row.indices[i]:min(row.indices[i] + rows.per.table - 1, nrow(importance.normalized)),,drop = FALSE], caption = cap, label =lab)
+    latex.vector = c(latex.vector, print(importance.normalized_latex))
+}
+all.latex = paste(latex.vector, collapse = "\n")
+writeLines(all.latex, con = "importance.tex")
+
+# importance.normalized_latex = print(importance.normalized_latex)
+# importance.normalized_latex = gsub(importance.normalized_latex, pattern = "\\begin{table}[ht]\n\\centering\n", replacement = "", fixed = TRUE)
+# importance.normalized_latex = gsub(importance.normalized_latex, pattern = "\\end{table}\n", replacement = "", fixed = TRUE)
+# importance.normalized_latex = gsub(importance.normalized_latex, pattern = "\\begin{tabular}{rr}", replacement = "\\begin{longtable}{| p{.20\\textwidth} | p{.80\\textwidth} |} ", fixed = TRUE)
+# importance.normalized_latex = gsub(importance.normalized_latex, pattern = "\\end{tabular}\n}", replacement = "", fixed = TRUE)
+# importance.normalized_latex = paste0(importance.normalized_latex, "\n\\end{longtable}")
+
 # Parameter Tuning based on caret package Choose Valdation Method
 CrossValidation = trainControl(method = "repeatedcv", number = nfolds, p = ptrain, repeats = nrepeats)
 # Create Random Forest Tuning Grid:
@@ -55,14 +71,14 @@ save(rftuned, file = "rf.RData")
 # Analyse Random Forest Results
 rfresults         = rftuned$results
 # Export Results as Latex Table
-rfresults_latex   = xtable(rfresults)
+rfresults_latex   = xtable(rfresults, caption = "Random Forest tuning results", label = "tab:rfresults")
 print(rfresults_latex, file = "rfresults.tex")
 # Plot RMSE vs mtry
-rfrmse = ggplot(data = rfresults, aes(x = mtry, y = RMSE)) + geom_line() + ggtitle("Root Mean Squared Error vs. Tuning Parameter mtry") + 
+rfrmse = ggplot(data = rfresults, aes(x = mtry, y = RMSE)) + geom_line() + theme_classic() +  ggtitle("Root Mean Squared Error vs. Tuning Parameter mtry") + 
     xlab("mtry") + ylab("RMSE")
 ggsave(rfrmse, filename = "rf_rmse.pdf", width = 20, height = 10, units = 'cm')
 # Plot RSquared vs mtry
-rfrsq = ggplot(data = rfresults, aes(x = mtry, y = Rsquared)) + geom_line() + ggtitle("R Squared vs. Tuning Parameter mtry") + 
+rfrsq = ggplot(data = rfresults, aes(x = mtry, y = Rsquared)) + geom_line() + theme_classic() +  ggtitle("R Squared vs. Tuning Parameter mtry") + 
     xlab("mtry") + ylab("R Squared")
 ggsave(rfrsq, filename = "rf_rsq.pdf", width = 20, height = 10, units = 'cm')
 # Tune Gradient Boosting Machine Create Tuning Grid
@@ -80,14 +96,14 @@ save(gbmtuned, file = "gbm.RData")
 gbmresults              = gbmtuned$results
 gbmresults$n.trees      = as.factor(gbmresults$n.trees)
 # Export GBM Tuning Results as latex table Export Results as Latex Table
-gbmresults_latex = xtable(gbmresults)
+gbmresults_latex = xtable(gbmresults, caption = "Gradient Boosting Machine tuning results", label = "tab:gmbresults")
 print(gbmresults_latex, file = "gbmresults.tex")
 # Plot RMSE vs. shrinkage for each value of n.trees
-gbmrmse = ggplot(data = gbmresults, aes(x = shrinkage, y = RMSE, colour = n.trees)) + geom_line() + ggtitle("Root Mean Squared Error vs. Tuning Parameter Shrinkage") + 
+gbmrmse = ggplot(data = gbmresults, aes(x = shrinkage, y = RMSE, colour = n.trees)) + geom_line() + theme_classic() +  ggtitle("Root Mean Squared Error vs. Tuning Parameter Shrinkage") + 
     xlab("Shrinkage") + ylab("RMSE")
 ggsave(gbmrmse, filename = "gbm_rmse.pdf", width = 20, height = 10, units = 'cm')
 # Plot RSquared vs. shrinkage for each value of n.trees
-gbmrsq = ggplot(data = gbmresults, aes(x = shrinkage, y = Rsquared, col = n.trees)) + geom_line() + ggtitle("R Squared vs. Tuning Parameter Shrinkage") + 
+gbmrsq = ggplot(data = gbmresults, aes(x = shrinkage, y = Rsquared, col = n.trees)) + geom_line() + theme_classic() +  ggtitle("R Squared vs. Tuning Parameter Shrinkage") + 
     xlab("Shrinkage") + ylab("R Squared")
 ggsave(gbmrsq, filename = "gbm_rsq.pdf", width = 20, height = 10, units = 'cm')
 #Visualise single regression tree for illustration in Report
